d052cb0a060e4233fd69990bc3a42a7823f9babb
Nksama <75163978+Nksama@users.noreply.github.com>
2021-02-03 18:11:11 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -5,7 +5,7 @@ from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, Inl
 async def start(client, message):
     # return
     joinButton = InlineKeyboardMarkup([
-        [InlineKeyboardButton("Channel", url="https://t.me/tonikawa123sub")],
+        
         [InlineKeyboardButton(
             "Report Bugs 😊", url="https://t.me/nk_guy")]
     ])
